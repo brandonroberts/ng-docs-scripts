ab55fb0774392341b665f66789a1036165fca54b
Brandon <robertsbt@gmail.com>
2018-12-06 18:29:49 +0000

    Pull github token from .env file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-const token = process.env.GITHUB_API_KEY;
 const octokit = require('@octokit/rest')();
+const dotenv = require('dotenv');
 
-octokit.authenticate({ type: 'token', token });
+dotenv.config();
+
+const token = process.env.GITHUB_TOKEN;
+
+async function addLabels(prNumber: number, labels: string[], owner = 'brandonroberts', repo = 'angular') {
+  const result = await octokit.issues.addLabels({owner, repo, number: prNumber, labels});
+
+  console.log('Result', result);
+}
+
+function main() {
+  console.log('authenticate');
+  octokit.authenticate({ type: 'token', token });
+
+  console.log('add label');
+  addLabels(1, ['enhancement']);
+}
+
+main();
